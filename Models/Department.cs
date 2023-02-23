ea129f2ebbe342db1c178942c6f2120d5bdb7df2
Muhammad Imad Khan Safi <113524214+mimadkhansafi@users.noreply.github.com>
2023-02-23 18:58:35 +0500

    Add project files.

---
+++ Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppApi.Models
+{
+    public class Department
+    {
+        public long DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
